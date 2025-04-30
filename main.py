b895ced1625ee4668fdc2b09f981db1b7ea84ca8
ali-the-pianostairs <alikhandwalla@outlook.com>
2025-04-30 13:31:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if "history" not in st.session_state:
     st.session_state.history = []  # list of (role, message, timestamp)
 
 # API configuration
-genai.configure(api_key=os.environ.get("API_KEY"))
+genai.configure(api_key="AIzaSyAyw9rCiJidfvnrvMbdjjkGkV55RM4nvCA")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Chat container
